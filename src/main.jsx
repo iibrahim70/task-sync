ea659f924e34ea6f9dbd5199cc53fbbe986d8341
iibrahim70 <iibrahiim.dev@gmail.com>
2023-09-07 21:30:28 +0600

    signup page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home.jsx";
+import Signin from "./pages/Signin.jsx";
+import Signup from "./pages/Signup.jsx";
 import "./index.css";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/signup", element: <Signup /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,87 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+
+const Signin = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const { signIn } = useContext(AuthContext);
+  // const { showToast } = useToast();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (user) => {
+    const { email, password } = user;
+    signIn(email, password)
+      .then(() => {
+        navigate(from, { replace: true });
+        // showToast("Welcome back to car craze!");
+        reset();
+      })
+      .catch((err) => {
+        // showToast(err.message);
+        console.error(err);
+      });
+  };
+
+  return (
+    <section className="w-[90%] md:w-[50%] mx-auto">
+      <div className="shadow-2xl p-10 flex items-center justify-center flex-col w-full min-h-screen">
+        <h2 className="text-center text-4xl font-bold mb-10 text-white">
+          Login
+        </h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <div className="mb-4">
+            <label className="block mb-1 font-medium">Email</label>
+            <input
+              className="inputFields"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium">Password</label>
+            <input
+              className="inputFields"
+              type="password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="bg-red-500 px-3 py-2 rounded w-full text-white"
+          >
+            Signin
+          </button>
+        </form>
+
+        <p className="w-full mt-4">
+          Don't have an account yet?{" "}
+          <Link className="text-blue-500" to="/signup">
+            Signup
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Signin;
