d60c1f35556d037dba1d9e6f572f8839e5fd2f6e
iibrahim70 <aviknuhash70@gmail.com>
2023-09-08 11:55:55 +0600

    added team colloab functionality

--- src/pages/TeamCollaboration.jsx
+++ src/pages/TeamCollaboration.jsx
@@ -40,47 +40,70 @@ const TeamCollaboration = () => {
   };
 
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen w-full text-black">
-      <h2 className="text-center text-2xl font-bold mb-10">
+    <main className="flex flex-col justify-center items-center min-h-screen w-full text-black p-10">
+      <h2 className="text-center text-4xl font-bold mb-10">
         Team Collaboration
       </h2>
 
-      <div>
-        <section>
-          <form onSubmit={handleSubmit(handleCreateTeam)}>
-            <h3 className="block mb-1 font-medium">Create a Team</h3>
-            <input
-              type="text"
-              className="inputFields mb-2"
-              placeholder="Team Name"
-              {...register("teamName")}
-            />
-            <button
-              type="submit"
-              className="bg-red-500 text-white px-2 py-1 rounded w-full"
-            >
-              Create
-            </button>
-          </form>
-        </section>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 ">
+        <div>
+          <section>
+            <form onSubmit={handleSubmit(handleCreateTeam)}>
+              <h3 className="block mb-1 font-medium">Create a Team</h3>
+              <input
+                type="text"
+                className="inputFields mb-2"
+                placeholder="Team Name"
+                {...register("teamName")}
+              />
+              <button
+                type="submit"
+                className="bg-red-500 text-white px-2 py-1 rounded w-full"
+              >
+                Create
+              </button>
+            </form>
+          </section>
 
-        <section className="mt-5">
-          <form onSubmit={handleSubmit(handleJoinTeam)}>
-            <h3 className="block mb-1 font-medium">Join a Team</h3>
-            <input
-              type="text"
-              className="inputFields mb-2"
-              placeholder="Team ID"
-              {...register("teamId")}
-            />
-            <button
-              type="submit"
-              className="bg-green-500 text-white px-2 py-1 rounded w-full"
-            >
-              Join
-            </button>
-          </form>
-        </section>
+          <section className="mt-5">
+            <form onSubmit={handleSubmit(handleJoinTeam)}>
+              <h3 className="block mb-1 font-medium">Join a Team</h3>
+              <input
+                type="text"
+                className="inputFields mb-2"
+                placeholder="Team ID"
+                {...register("teamId")}
+              />
+              <button
+                type="submit"
+                className="bg-green-500 text-white px-2 py-1 rounded w-full"
+              >
+                Join
+              </button>
+            </form>
+          </section>
+        </div>
+
+        <div>
+          <h3 className="block mb-5 font-medium text-center">Your Teams</h3>
+
+          <ul>
+            {teams.map((team) => (
+              <li
+                key={team.id}
+                className="space-y-2 flex items-center flex-col justify-center py-5 rounded shadow-xl mb-5"
+              >
+                Team: {team.name}
+                <li>ID: {team.id}</li>
+                <ul>
+                  {team.members.map((member) => (
+                    <li key={member}>Members: {member}</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
