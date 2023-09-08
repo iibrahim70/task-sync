c5f43eaa975b02f3ade130e5f997923056e3d26d
iibrahim70 <iibrahiim.dev@gmail.com>
2023-09-08 09:38:01 +0600

    added task creation page and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
-
-  const navItems = (
-    <div className="flex flex-col lg:flex-row gap-y-5 lg:gap-x-10 text-base font-semibold text-white/80">
-      <a>Home</a>
-      <a>Blog</a>
-      <a>About</a>
-      <a>Contact</a>
-    </div>
-  );
 
   const handleLogout = () => {
     logOut()
@@ -20,63 +11,67 @@ const Navbar = () => {
       .catch((err) => console.log(err));
   };
 
+  const navItems = (
+    <div className="flex flex-col items-center text-base font-semibold text-white/80 mt-5 ghotstBtn">
+      <Link to="/">Home</Link>
+      <Link to="/create-task">Task Management</Link>
+      <a>About</a>
+      <a>Contact</a>
+      {user ? (
+        <button onClick={handleLogout}>Signout</button>
+      ) : (
+        <Link to="/signin">Signin</Link>
+      )}
+      <hr className="border border-red-500 w-[50%] my-5" />
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-20 rounded-full">
+          <img src={user?.photoURL} />
+        </div>
+      </label>
+    </div>
+  );
+
   return (
     <section className="bg-base-300">
-      <div className="drawer wrapper">
+      <div className="drawer bg-slate-700 z-10 flex-none lg:hidden">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
-          <div className="w-full navbar p-0 grid grid-cols-3">
-            <div className="flex-none lg:hidden">
-              <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-6 h-6 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-
-            <div className="text-lg font-bold text-white/90">Task Sync</div>
-
-            <div className="hidden lg:flex justify-center">
-              <ul className="menu menu-horizontal p-0">{navItems}</ul>
-            </div>
-
-            <div className="hidden lg:flex justify-end">
-              <div className="dropdown dropdown-end flex justify-center items-center gap-5">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-10 rounded-full">
-                    <img src={user?.photoURL} />
-                  </div>
-                </label>
-                {user ? (
-                  <button
-                    onClick={handleLogout}
-                    className="btn btn-ghost btn-sm"
-                  >
-                    Signout
-                  </button>
-                ) : (
-                  <Link to="/signin" className="btn btn-ghost btn-sm">
-                    Signin
-                  </Link>
-                )}
-              </div>
-            </div>
+          <div className="w-full navbar bg-base-300">
+            <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+            <div className="flex mx-auto font-semibold text-xl">Task Sync</div>
           </div>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200">{navItems}</ul>
+          <ul className="menu p-0 w-80 h-full bg-base-200">{navItems}</ul>
+        </div>
+      </div>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center bg-white min-h-screen">
+          {/* Page content here */}
+          <Outlet />
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-0 w-80 h-full bg-base-200 text-base-content">
+            {navItems}
+          </ul>
         </div>
       </div>
     </section>
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,19 @@ import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
 import "./index.css";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import TaskManager from "./pages/TaskManager.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/create-task",
+        element: <TaskManager />,
+      },
+    ],
+  },
   { path: "/signin", element: <Signin /> },
   { path: "/signup", element: <Signup /> },
 ]);
